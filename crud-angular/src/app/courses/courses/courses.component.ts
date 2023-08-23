1b0df70d7e0d0966bf1b90793e942d431d0350be
jpedrosantosb <jpedrosantosb@gmail.com>
2023-08-23 20:54:08 -0300

    Chamada HTTP Get no Angular e RXJS

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,7 +10,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable <Course[]>;
   displayedColumns = ['name', 'category']
 
   //coursesService: CoursesService;
