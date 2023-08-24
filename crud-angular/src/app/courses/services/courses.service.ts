0718ab439db0ac8ab387a971ab72b3590021ec85
jpedrosantosb <jpedrosantosb@gmail.com>
2023-08-23 21:18:03 -0300

    Spinner (Carregando)

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { first, tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +10,14 @@ export class CoursesService {
 
   private readonly API = '/assets/courses.json';
 
+
   constructor(private httpClient: HttpClient) { }
 
   list() {
     return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
+        delay(3000),
         tap(courses => console.log(courses))
       );
   }
