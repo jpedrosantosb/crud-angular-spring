4ad5cb4ce9ee1bd55a8824daa821a03f5f6a635e
jpedrosantosb <jpedrosantosb@gmail.com>
2023-08-24 23:58:56 -0300

    Tratamento de erro e pipes

---
+++ crud-angular/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppMaterialModule } from './app-material/app-material.module';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
+
+
+@NgModule({
+  declarations: [
+    ErrorDialogComponent,
+    CategoryPipe
+  ],
+  imports: [
+    CommonModule,
+    AppMaterialModule
+  ]
+})
+export class SharedModule { }
