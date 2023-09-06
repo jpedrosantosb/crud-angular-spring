39166d42e0b16c1fe919cd52d14e8c6776df1c59
jpedrosantosb <jpedrosantosb@gmail.com>
2023-09-06 00:23:43 -0300

    criar curso na API Spring

--- crud-spring/src/main/java/com/jpbastos/controller/CourseController.java
+++ crud-spring/src/main/java/com/jpbastos/controller/CourseController.java
@@ -3,9 +3,14 @@ package com.jpbastos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpbastos.model.CourseModel;
@@ -17,11 +22,26 @@ public class CourseController {
 
 	@Autowired
 	private CourseRepository courseRepository;
-	
-	//@RequestMapping(method = RequestMethod.GET) 
+
+	// @RequestMapping(method = RequestMethod.GET)
 	@GetMapping
 	public @ResponseBody List<CourseModel> list() {
 		return courseRepository.findAll();
 	}
-	
+
+//	// @RequestMapping(method = RequestMethod.POST)
+//	@PostMapping
+//	public ResponseEntity<CourseModel> create(@RequestBody CourseModel course) {
+//		// System.out.println(course.getName());
+//		// return courseRepository.save(course);
+//		return ResponseEntity.status(HttpStatus.CREATED)
+//				.body(courseRepository.save(course));
+//	}
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public CourseModel create(@RequestBody CourseModel course) {
+		return courseRepository.save(course);
+
+	}
 }
