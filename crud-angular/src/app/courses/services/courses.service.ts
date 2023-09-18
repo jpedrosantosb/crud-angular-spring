716306fb4cd314e5b05fec503c466c6006ad5fd9
jpedrosantosb <jpedrosantosb@gmail.com>
2023-09-17 23:52:26 -0300

    salvar form para criação e edição

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -24,10 +24,24 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
-    return this.httpClient.post<Course>(this.API, record).pipe(first());
+    console.log(record)
+    if (record._id) {
+      console.log('update')
+      return this.update(record)
+    }
+    console.log('create')
+    return this.create(record)
   }
 
   loadById(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  private create(record: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
