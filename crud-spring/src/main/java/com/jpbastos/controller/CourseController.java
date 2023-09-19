20c419455f73117dde1072c1db7a5c9d80ae22a1
jpedrosantosb <jpedrosantosb@gmail.com>
2023-09-18 23:54:36 -0300

    HTTP PUT - update

--- crud-spring/src/main/java/com/jpbastos/controller/CourseController.java
+++ crud-spring/src/main/java/com/jpbastos/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,8 +49,18 @@ public class CourseController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<CourseModel> findById(@PathVariable Long id) {
-		return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record))
+		return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
 				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<CourseModel> update(@PathVariable Long id, @RequestBody CourseModel course) {
+		return courseRepository.findById(id).map(recordFound -> {
+			recordFound.setName(course.getName());
+			recordFound.setCategory(course.getCategory());
+			CourseModel updated = courseRepository.save(recordFound);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.notFound().build());
 	}
 
 }
